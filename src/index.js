a6608b62618fd912b9c86bd6691db4e162a5a696
oilnur <oilnur@localhost.localdomain>
2018-05-04 17:32:10 +0300

    first edition cfgs

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import $ from 'jquery';
+
+$('.h1').html('Some text!')
+
+console.log("test!!!")
+
+import './css/style.css'
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+let path = require('path')
+let conf = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'main.js',
+        publicPath: 'dist/',
+    },
+    devServer: {
+        overlay: true
+    },
+    module: {
+        rules: [
+            {
+               test: /\.js$/,
+               loader: 'babel-loader' ,
+               //exclude: '/node_modules/'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+                //exclude: '/node_modules/'
+             }
+        ]
+    }
+};
+
+module.exports = (env, options) => {
+    let production = options.mode == 'production';
+    // 'eval-sourcemap' - быстро строит sourcemap 
+    // 'sourcemap'
+
+    conf.devtool = production
+                    ?  false 
+                    : 'eval-sourcemap';
+    return conf;
+}
+
+module.exports = conf;
